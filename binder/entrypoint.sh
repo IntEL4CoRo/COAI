99ae5d03fd23d8478d99d97a9c814d5cf3d3ac0e
Yanxiang Zhan <yanxiang@uni-bremen.de>
2023-09-29 16:35:34 +0200

    update base image

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -5,11 +5,4 @@ source ${HOME}/workspace/ros/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 
-# jupyter lab workspaces import ${HOME}/.binder/workspace.json
-
-# Use xvfb virtual display when there is no display connected.
-if [ -n "$DISPLAY" ]; then
-    exec "$@"
-else
-    xvfb-run exec "$@"
-fi
+exec "$@"
