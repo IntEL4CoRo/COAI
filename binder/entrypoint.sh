da9d3a8715a1f2702d40316d46958d5422f31662
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-03-07 14:02:54 +0100

    cleaning up local development setup

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -5,9 +5,6 @@ source ${HOME}/workspace/ros/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 
-# Create a symbolic link to the ROS workspace
-ln -s ${ROS_WS} $PWD/ros_ws
-
 # Use xvfb virtual display when there is no display connected.
 if [ -n "$DISPLAY" ]; then
     exec "$@"
