7465dedbe10f51120273b88d5de40c2b78036c28
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-03-07 13:54:17 +0100

    fix entrypoint path

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -5,7 +5,8 @@ source ${HOME}/workspace/ros/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 
-jupyter lab workspaces import ${HOME}/.binder/workspace.json
+# Create a symbolic link to the ROS workspace
+ln -s ${ROS_WS} $PWD/ros_ws
 
 # Use xvfb virtual display when there is no display connected.
 if [ -n "$DISPLAY" ]; then
