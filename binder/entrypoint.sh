6fec3a07b8568ad3ab0d2db0de25b15762a198c8
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-03-12 14:38:28 +0100

    Disable the xpra desktop completely

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -5,9 +5,9 @@ source ${HOME}/workspace/ros/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 
-Use xvfb virtual display when there is no display connected.
-if [ -n "$DISPLAY" ] && [ "$DISPLAY" != ":100" ]; then
-    exec "$@"
-else
+# # Use xvfb virtual display when there is no display connected.
+# if [ -n "$DISPLAY" ] && [ "$DISPLAY" != ":100" ]; then
+#     exec "$@"
+# else
 xvfb-run exec "$@"
-fi
+# fi
