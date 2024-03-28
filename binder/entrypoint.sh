061ca1bd36183c64f0412b1fe7df632a8b3b72d1
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-03-28 14:11:29 +0100

    update some UI settings

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-source ${HOME}/workspace/ros/devel/setup.bash
+source ${ROS_WS}/devel/setup.bash
 
 roscore &
 roslaunch --wait rvizweb rvizweb.launch &
+
+jupyter lab workspaces import  ${REPO_DIR}/binder/jupyterlab-workspace.json
 
 # # Use xvfb virtual display when there is no display connected.
 # if [ -n "$DISPLAY" ] && [ "$DISPLAY" != ":100" ]; then
