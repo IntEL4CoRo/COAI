ec7f5f5689e8789e947d180ec013eebec95c25a1
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-06-24 11:06:28 +0200

    update popcorn demo

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -5,4 +5,6 @@ source ${HOME}/workspace/ros/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 
+jupyter lab workspaces import  ${REPO_DIR}/binder/jupyterlab-workspace.json
+
 exec "$@"
